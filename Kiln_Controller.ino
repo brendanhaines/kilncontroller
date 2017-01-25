ffb56e01daf5064163593338545abb6753a4087c
Brendan Haines <brendan.haines@gmail.com>
2017-01-24 23:05:59 -0700

    Output from MCP23017 working

--- Kiln_Controller.ino
+++ Kiln_Controller.ino
@@ -12,40 +12,51 @@ MAX6675 t0(CLK0, CS0, MISO0);
 Adafruit_MCP23017 mcp;
 
 void setup() {
-  Wire.pins(4,5);
-  
+  // LED
+  pinMode(2, OUTPUT);
+
+  // I2C
+  Wire.pins(4, 5);
+
   // LCD
   lcd.init();
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Initializing... ");
 
   // MCP23017 GPIO expander
   mcp.begin();
   mcp.pinMode(0, OUTPUT);
   mcp.digitalWrite(0, HIGH);
+  digitalWrite(2, LOW);
   delay(100);
   mcp.digitalWrite(0, LOW);
+  digitalWrite(2, HIGH);
 
   // Clear LCD
-  lcd.setCursor(0,0);
-  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  lcd.clear();
 }
 
 void loop() {
   // Get value
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print(t0.readFahrenheit());
+  lcd.print("F ");
+  lcd.print(t0.readCelsius());
+  lcd.print("C");
 
   // LED stuff
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("LED: ON ");
   mcp.digitalWrite(0, HIGH);
-  delay(1000);
-  lcd.setCursor(0,1);
+  digitalWrite(2, LOW);
+  delay(2000);
+  lcd.setCursor(0, 1);
   lcd.print("LED: OFF");
   mcp.digitalWrite(0, LOW);
-  delay(1000);
-  
+  digitalWrite(2, HIGH);
+  delay(2000);
+
 }
